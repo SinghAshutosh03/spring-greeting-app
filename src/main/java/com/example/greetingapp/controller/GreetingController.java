9545b19cd71aefa8671c867218ad4cf2c1bf4cfb
SinghAshutosh03 <ashutoshsingh030302@gmail.com>
2025-03-04 14:34:51 +0530

    Added UC2: GreetingController now uses GreetingService

--- src/main/java/com/example/greetingapp/controller/GreetingController.java
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -1,13 +1,36 @@
-package com.example.greetingapp.controller;
+//package com.example.greetingapp.controller;
+//
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/greeting")
+//public class GreetingController {
+//
+//    @GetMapping("/hello")
+//    public String sayHello() {
+//        return "{ \"message\": \"Hello from BridgeLabz\" }";
+//    }
+//}
 
+package com.example.greetingapp.controller;
+//package com.example.greetingapp.service;
+
+
+import com.example.greetingapp.service.GreetingService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
-        return "{ \"message\": \"Hello from BridgeLabz\" }";
+        return greetingService.getGreetingMessage();
     }
 }
